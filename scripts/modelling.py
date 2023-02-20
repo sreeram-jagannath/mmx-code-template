b973adf220b0e62ed20e68d061bc7d1d9bb08d47
Sreeram Jagannath <sreeram.subbaray@tigeranalytics.com>
2023-02-20 10:50:33 +0530

    added priors and model config

--- scripts/modelling.py
+++ scripts/modelling.py
@@ -59,57 +59,60 @@ def _get_bambi_predictions(pred_object):
 
 
 def train_bayesian(
-    X_train: pd.DataFrame, X_test: pd.DataFrame, y_train: pd.Series, y_test: pd.Series, priors=None
-) -> tuple[pd.DataFrame, pd.Series, pd.Series, BaseEstimator]:
+    X_train: pd.DataFrame, 
+    X_test: pd.DataFrame, 
+    y_train: pd.Series, 
+    y_test: pd.Series, 
+    priors_config: dict = None,
+    model_args: dict = None,
+) -> tuple[pd.DataFrame, pd.Series, pd.Series, bmb.Model]:
     """
-    Fits a Bayesian linear regression model on the train data and returns a pandas dataframe with feature names
+    Fits a Bayesian linear regression model on the train data and returns a pandas dataframe with feature names 
     and feature coefficients, train predictions, test predictions, and the trained model object.
-
+    
     Args:
         X_train: A pandas DataFrame containing the feature values for the training data.
         X_test: A pandas DataFrame containing the feature values for the test data.
         y_train: A pandas Series containing the target values for the training data.
         y_test: A pandas Series containing the target values for the test data.
-        priors: A dictionary with prior distributions for the model parameters. Defaults to None.
-
+        priors_config: A dictionary with prior distributions for the model parameters. Defaults to None.
+    
     Returns:
         A tuple containing the following:
-        - The trained Bayesian linear regression model object.
         - A pandas dataframe with two columns containing feature names and feature coefficients.
         - A pandas Series with the train predictions.
         - A pandas Series with the test predictions.
+        - The trained Bayesian linear regression model object.
     """
     # Remove all punctuation and spaces from the column names
-    translator = str.maketrans("", "", string.punctuation + " ")
+    translator = str.maketrans('', '', string.punctuation + ' ')
     X_train.columns = [col.translate(translator) for col in X_train.columns]
     X_test.columns = [col.translate(translator) for col in X_test.columns]
 
     # Add the target variable to the X_train and X_test dataframes
     X_train = X_train.copy()
-    X_train["target"] = y_train
+    X_train['target'] = y_train
     X_test = X_test.copy()
-    X_test["target"] = y_test
-
+    X_test['target'] = y_test
+    
     # Define the model using Bambi
-    model_equation = "target ~ " + " + ".join(X_train.columns[:-1])
+    model_equation = 'target ~ ' + ' + '.join(X_train.columns[:-1])
     model = bmb.Model(model_equation, data=X_train)
-    if priors is not None:
-        model.set_priors(priors)
-
+    if priors_config is not None:
+        model.set_priors(priors_config)
+    
     # Fit the model using MCMC sampling
-    trace = model.fit(draws=5, cores=1, chains=2, tune=5)
-
+    trace = model.fit(**model_args)
+    
     # Get the posterior distribution of the model coefficients
-    feature_df = az.summary(trace)["mean"].reset_index().query("~index.str.contains('y_mean')")
-    feature_df = feature_df.rename(
-        columns={"index": "feature names", "mean": "feature coefficients"}
-    )
-
+    feature_df = az.summary(trace)['mean'].reset_index().query("~index.str.contains('y_mean')")
+    feature_df = feature_df.rename(columns={ "index": "feature names", "mean": "feature coefficients" })
+    
     # Make predictions on the training and test data
     train_preds_object = model.predict(idata=trace, data=X_train, inplace=False)
     test_preds_object = model.predict(idata=trace, data=X_test, inplace=False)
-
+    
     train_preds = _get_bambi_predictions(pred_object=train_preds_object)
     test_preds = _get_bambi_predictions(pred_object=test_preds_object)
-
+    
     return model, feature_df, train_preds, test_preds
